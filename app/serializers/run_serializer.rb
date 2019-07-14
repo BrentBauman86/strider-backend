864b30bb402e705231d7e580870933ef195cdccd
BrentBauman86 <brentbauman86@gmail.com>
2019-07-14 14:49:23 -0700

    create sessions controller and move runs and users to an apifolder

--- app/serializers/run_serializer.rb
+++ app/serializers/run_serializer.rb
@@ -1,8 +1,8 @@
 class RunSerializer < ActiveModel::Serializer
-  attributes :id, :name, :distance, :date, :comment, :owner
+  attributes :id, :name, :distance, :date, :comment, :user_id
   
-  def owner
-    {owner_id: self.object.user.id,
-    owner_name: self.object.person.name}
-  end 
+  # def owner
+  #   {owner_id: self.object.user.id,
+  #   owner_name: self.object.person.name}
+  # end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :runs
-  resources :users
+  namespace :api do 
+    resources :runs
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
