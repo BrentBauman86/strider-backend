df8fe5a5a99ceafb545794e49077061edfe2e69e
BrentBauman86 <brentbauman86@gmail.com>
2019-08-20 02:30:49 -0700

    session destroy success message

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,3 +1,32 @@
-class SessionsController < ApplicationController
+class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by(name: params[:session][:name])
+        if @user && @user.authenticate(params[:session][:password])
+            session[:id] = @user.id
+            render json: @user 
+        else 
+            render json: {
+                error: "Invalid Credentials"
+            }
+        end
+    end
+
+    def get_current_user
+        if logged_in?
+            render json: current_user 
+        else
+            render json: {
+                error: "No one logged in"
+            }
+        end
+    end
+
+    def destroy
+        session.clear 
+        render: json: {
+            notice: "successfully logged out"
+        }, status: :ok 
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/api/login', to: 'sessions#create'
+  post '/api/login', to: "sessions#create"
+  delete '/api/logout', to: "sessions#destroy"
+
+
   namespace :api do 
     resources :runs
     resources :users
